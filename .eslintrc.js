10d2c6db2190592e348d8942f8854d1671783921
aaron <120069668@qq.com>
2022-10-17 14:40:45 +0800

    feat: engineering the project,add prettier eslint editorconfig commitizen

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  extends: 'standard',
+  rules: {}
+}
