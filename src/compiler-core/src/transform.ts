2ed527e875c9664b887f8f176e3459e7e5ed3b84
aaron <120069668@qq.com>
2023-02-25 16:20:56 +0800

    feat: add compiler-core

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,35 @@
+export function transform(root, options) {
+  const context = createTransformContext(root, options)
+
+  traverseNode(root, context)
+}
+
+function traverseNode(node, context) {
+  const nodeTransforms = context.nodeTransforms
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i]
+    transform(node)
+  }
+
+  traverseChildren(node, context)
+}
+
+function traverseChildren(node, context) {
+  const children = node.children
+
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i]
+      traverseNode(node, context)
+    }
+  }
+}
+
+function createTransformContext(root, options) {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || []
+  }
+
+  return context
+}
